a85dece3e5f70527957f61410d50ee0397d6a602
Bram <bram.v@cervus.ai>
2025-03-26 12:57:53 +0000

    updated the running script to pull second time around

--- test-rover.bat
+++ test-rover.bat
@@ -1,3 +1,10 @@
-git clone https://github.com/bramvandermeersch/mars-rover-team.git
-cd mars-rover-team
-python .\src\run_rover.py
+if exist mars-rover-team\ (
+    cd mars-rover-team
+    git pull
+    cd ..
+) else (
+    git clone https://github.com/bramvandermeersch/mars-rover-team.git
+)
+
+python mars-rover-team\src\run_rover.py
+pause
